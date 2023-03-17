7ca95b5a42a51ee8f40ac307a020f333de34d5e1
Peter Liu <lp49@qq.com>
2023-03-18 02:14:23 +0800

    add arouter

--- app/src/main/java/com/basic/app/MainActivity2.kt
+++ app/src/main/java/com/basic/app/MainActivity2.kt
@@ -2,7 +2,9 @@ package com.basic.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alibaba.android.arouter.facade.annotation.Route
 
+@Route(path = "/app/main")
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
